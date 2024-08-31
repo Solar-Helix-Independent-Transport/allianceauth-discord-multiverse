a8cb1c50c06a9a6f053c7048c2943700af17abfb
Joel Falknau <joel.falknau@gmail.com>
2024-08-31 23:42:47 +1000

    add docstring description to project

--- aadiscordmultiverse/__init__.py
+++ aadiscordmultiverse/__init__.py
@@ -1,2 +1,6 @@
+"""
+Service module for managing an unlimited number of discord servers from a single auth instance. It can run side by side with the core auth discord service, or completely standalone.
+"""
 __version__ = "0.0.1a5"
 __branch__ = "ALPHA"
+__title__ = "Discord Multiverse"
