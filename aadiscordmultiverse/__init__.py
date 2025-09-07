e3852537b23ea7fdd40332628137bb4f200869b5
Aaron Kable <aaronkable@gmail.com>
2025-09-07 14:16:29 +0800

    version

--- aadiscordmultiverse/__init__.py
+++ aadiscordmultiverse/__init__.py
@@ -1,6 +1,6 @@
 """
 Service module for managing an unlimited number of discord servers from a single auth instance. It can run side by side with the core auth discord service, or completely standalone.
 """
-__version__ = "0.0.1b1"
+__version__ = "0.0.1b2"
 __branch__ = "beta"
 __title__ = "Discord Multiverse"
